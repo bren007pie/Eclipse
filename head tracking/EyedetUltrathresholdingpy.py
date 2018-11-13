679035fa6ef12fcb689e11c765246606ec9bfc2d
bren007pie <bflawther@live.com>
2018-11-13 16:43:58 -0500

    Added distance mapping to main program

--- head tracking/EyedetUltrathresholdingpy.py
+++ head tracking/EyedetUltrathresholdingpy.py
@@ -13,6 +13,7 @@ from imutils.video import FPS
 import RPi.GPIO as GPIO
 import time
 from threading import Timer
+import math as m
 
 #global variable setup
 #Sets up Camera and OpenCV and FPS
@@ -120,16 +121,46 @@ def getfacecentre(faces,debug): #gets centre of the face and returns it as an x 
         print("Face centre:\n", centres)
     return centres
 
-def geteyecentres(eyes, debug)
+def geteyecentres(eyes, debug):
     centres = []
 
     return centres
 
 
-def geteyedistance(eyecentres, dist, debug)
+def geteyedistance(eyecentres, dist, debug):
+    #FOV 60 degrees (assume both directions but test) #https://support.logitech.com/en_us/article/17556
+    #example eyecentres
+    #eyecentres = [[100, 300], [200,300]] #left and right eye
+    #dist = 20 #person is 20 cm away
+    #resolution = 1280 in x 720 in y
     #FOV 60 degrees (assume both directions but test) #https://support.logitech.com/en_us/article/17556
 
+    #My way
+    eyedist = []
+    FOVx = 60*m.pi/180 #FOV in radians
+    thetax = FOVx/2
+    Resolutionx = 1280
+
+    objectplanex = dist*m.tan(thetax)
+    mapx = objectplanex/Resolutionx #gets cm/pixel at that distance
+
+
+    FOVy = 60*m.pi/180 #FOV in radians
+    thetay = FOVx/2
+    Resolutiony = 720
+
+    objectplaney = dist*m.tan(thetay)
+    mapy = objectplaney/Resolutiony #gets cm/pixel at that distance
+
+    for (x,y) in eyecentres:
+        eyedist.append([mapx*x, mapy*y])
+    if debug:
+        #print(eyecentres)
+        print("Eye Maps:\n", mapx, mapy)
+        print("Eye Distances:\n",eyedist)
     return eyedist
+
+
     
 
 
@@ -178,7 +209,7 @@ while 1:
                 eyecentres.append([x+ex+ew/2, y+ey+eh/2])
             print("EyeCentres:\n", eyecentres)
 
-
+            eyedistance = geteyedistance(eyecentres, 20*2.54, True)
 
             #finding the number of eyes, should also just be a function
             print("Eye array: \n", eyes)
@@ -262,7 +293,7 @@ while 1:
         k = cv2.waitKey(30) & 0xff
         if k == 27: #press escape key to escape
             break
-
+ 
 
         fps.update() #updates the fps object each time the detection runs
 
