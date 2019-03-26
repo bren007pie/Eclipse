8c4a77cdda5f45d905c6e13a43d7484a0d811102
Brendan Fallon <bflawther@live.com>
2019-03-26 15:08:13 -0400

    HAHAHAHAHA

---
+++ bottomhalf.py
@@ -0,0 +1,92 @@
+import time
+import adafruit_mcp230xx
+import board
+import busio
+import digitalio
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(0)
+channels = list(range(4,14)) + list(range(16,18))
+GPIO.setup(channels, GPIO.OUT, initial = 0)
+
+#set up "y-channels", i.e. the rows
+ychannel1 = channels[0:3]
+ychannel2 = channels[3:6]
+ychannel3 = channels[6:9]
+ychannel4 = channels[9:12]
+
+#necessary initialization for the first column of each quadrant
+GPIO.output(ychannel1[0],1)
+GPIO.output(ychannel2[0],1)
+GPIO.output(ychannel3[0],1)
+GPIO.output(ychannel4[0],1)
+
+
+##i2c = busio.I2C(board.SCL, board.SDA)
+##mcp1 = adafruit_mcp230xx.MCP23017(i2c, address = 0x20)
+##mcp3 = adafruit_mcp230xx.MCP23017(i2c, address = 0x22)
+##
+##quad1 = []
+##quad3 = []
+##
+##for i in range(0,6):
+##    quad1.append(mcp1.get_pin(i))
+##    quad3.append(mcp3.get_pin(i))
+##    quad1[i].direction = digitalio.Direction.OUTPUT
+##    quad3[i].direction = digitalio.Direction.OUTPUT
+##
+###assuming parameters are being received from another part of the code
+##def blockTile(x,y,prev_x,prev_y):
+##    #check if new coords are the same as the last. If not then proceed.
+##
+##    try:
+##        if x != prev_x or y !=prev_y:
+##            #assuming no error catching is necessary because x and y are
+##            #carefully prepared in another part of the code
+##            if x < 6:
+##                if y < 3:
+##                    for i in range(6): #range of 6 (0,1,2,3,4,5) should be enough?
+##                        quad1[i].value = 0
+##                    GPIO.output(ychannel1[y],1)
+##                    for i in range(3):
+##                        if i != y:
+##                            GPIO.output(ychannel1[i],0)
+##                    quad1[x].value = 1
+##                    print('done did it!!!')
+##
+##                else:
+##                    for i in range(6): 
+##                        quad2[i].value = 0
+##                    GPIO.output(ychannel2[y-3],1)
+##                    for i in range(3):
+##                        if i+3 != y:
+##                            GPIO.output(ychannel2[i],0) #check with Stevie
+##                    quad2[x].value = 1
+##                    print('yah y33t')
+##
+##            else:
+##                if y < 3:
+##                    for i in range(6): #range of 6 (0,1,2,3,4,5) should be enough?
+##                        quad3[i].value = 0 
+##                    GPIO.output(ychannel3[y],1)
+##                    for i in range(3):
+##                        if i != y:
+##                            GPIO.output(ychannel3[i],0)
+##                    quad3[x-6].value = 1
+##                    print('flicka da wrist boi')
+##
+##                else:
+##                    for i in range(6): #range of 6 (0,1,2,3) should be enough?
+##                        quad4[i].value = 0
+##                    GPIO.output(ychannel4[y-3],1)
+##                    for i in range(3):
+##                        if i+3 != y:
+##                            GPIO.output(ychannel4[i],0)
+##                    quad4[x-6].value = 1
+##                    print('do whatcha daddy did')
+##
+##        else:
+##            print('same tile as before!')
+##    except KeyboardInterrupt:
+##        GPIO.cleanup()
--- optimization_control.py
+++ optimization_control.py
@@ -19,7 +19,6 @@ import subprocess
 
 ##THIS VERSION OF THE INTEGRATED CODE HAS GRID ADDRESSING IN A SEPARATE THREAD, FOR A TOTAL OF 3 THREADS##
 
-
 subprocess.call("./pre_eclipse.sh", shell=True)
 
 #change system path to be able to import sunlight sensor driver
@@ -37,9 +36,6 @@ subprocess.call("./pre_eclipse.sh", shell=True)
 #	return (vis,IR,UV)
 
 
-
-
-#Define threaded Pi Cam class
 class PiCamThreaded:
     def __init__(self, resolution = (640,480),framerate = 16):
         #initialize camera and stream
@@ -47,7 +43,7 @@ class PiCamThreaded:
         self.camera.resolution = resolution
         self.camera.framerate = framerate
         self.camera.exposure_mode = 'off' #'auto'
-        self.camera.shutter_speed = 125#250
+        self.camera.shutter_speed = 250
         #self.camera.iso = 100 #try this low iso
         self.rawCapture = PiRGBArray(self.camera, size = resolution)
         self.stream = self.camera.capture_continuous(self.rawCapture,format = "bgr", use_video_port=True)
@@ -111,10 +107,15 @@ class GridAddressing:
         return self
 
     def update(self):
-        #global alpha_other
+        global phi_other
+        
         prev_x1 = None
+        prev_x2 = None
+        prev_y1 = None
+        prev_y2 = None
+        
         while True:
-             #constants and variables
+            #constants and variables
             xCam1 = 55
             yCam2 = 50
             dSensorToWindshield = 20
@@ -122,11 +123,13 @@ class GridAddressing:
 
             #outputs from Light Tracking (in deg)
 
-            if alpha[0]:
+            if alpha[0] and phi_other[0]:   #serial MUST be connected to work             
                 alpha1 = alpha[0]#alpha_other[0] #-10
-                phi1 = -15+ phi[0]#phi[0] #-15#
+                phi1 = phi_other[0]#phi[0] #-15#
+
                 alpha2 = alpha[1]#None#60
-                phi2 = phi[1]#None #0
+                if phi_other[1]:
+                    phi2 = phi_other[1]#-15 +phi_other[1]#-15 + phi_other[1]#None #0
             else:
                 alpha1 = None
                 phi1 = None
@@ -137,9 +140,9 @@ class GridAddressing:
             #eye_x and eye_y must be mirrored to get the correct
             #displacement from the perspective of the driver
             horizontalDisplacement = -eye_x 
-            print('hD: ' + str(horizontalDisplacement))
+            #print('hD: ' + str(horizontalDisplacement))
             verticalDisplacement = -eye_y
-            print('vD: ' + str(verticalDisplacement))
+            #print('vD: ' + str(verticalDisplacement))
             depthSensorToDriver = d_dist
 
             x1 = y1 = x2 = y2 = x3 = y3 = x4 = y4 = None
@@ -162,7 +165,7 @@ class GridAddressing:
                     x1 = column1
                     y1 = row1
                 
-                if type(alpha2) != type(None):
+                if type(alpha2) != type(None) and type(phi2) != type(None):
                     xProjection2 = dDriver*math.tan(math.radians(alpha2)) + xDriver
                     yProjection2 = dDriver*math.tan(math.radians(phi2)) + yDriver
                     
@@ -177,38 +180,37 @@ class GridAddressing:
             #begin actual grid addressing
             if type(x1) != type(None):
                 if y1 == 1:
+                    #if location changed in y or x, make previous transparent
+                    if prev_x1 != x1 and type(prev_x1) != type(None):
+                        GPIO.output(self.bottomRowPins[prev_x1],0)
+                    if prev_y1 != y1 and type(prev_y1) != type(None):
+                        GPIO.output(self.topRowPins[prev_x1],0)
+                    #make new one opaque
                     GPIO.output(self.bottomRowPins[x1],1)
-                    
+                elif y1 == 2:                   
                     if prev_x1 != x1 and type(prev_x1) != type(None):
-                        GPIO.output(self.bottomRowPins[prev_x1],0)
-                    if prev_y1 != y1 and type(prev_y1) != type(None):
                         GPIO.output(self.topRowPins[prev_x1],0)
+                    if prev_y1 != y1 and type(prev_y1) != type(None):
+                        GPIO.output(self.bottomRowPins[prev_x1],0)
                         
-                elif y1 == 2:
                     GPIO.output(self.topRowPins[x1],1)
-                    
-                    if prev_x1 != x1 and type(prev_x1) != type(None):
-                        GPIO.output(self.topRowPins[prev_x1],0)
-                    if prev_y1 != y1 and type(prev_y1) != type(None):
-                        GPIO.output(self.bottomRowPins[prev_x1],0)
-                        
                     
                 if type(x2) != type(None): #second light!
                     if y2 == 1:
-                        GPIO.output(self.bottomRowPins[x2],1)
-
                         if prev_x2 != x2 and type(prev_x2) != type(None):
                             GPIO.output(self.bottomRowPins[prev_x2],0)
                         if prev_y2 != y2 and type(prev_y2) != type(None):
-                            GPIO.output(self.topRowPins[prev_y2],0)
+                            GPIO.output(self.topRowPins[prev_x2],0)
+
+                        GPIO.output(self.bottomRowPins[x2],1)
                       
                     elif y2 == 2:
-                        GPIO.output(self.topRowPins[x2],1)
-
                         if prev_x2 != x2 and type(prev_x2) != type(None):
                             GPIO.output(self.topRowPins[prev_x2],0)
                         if prev_y2 != y2 and type(prev_y2) != type(None):
-                            GPIO.output(self.bottomRowPins[prev_y2],0)
+                            GPIO.output(self.bottomRowPins[prev_x2],0)
+
+                        GPIO.output(self.topRowPins[x2],1)
 
                 #else:
                 #    print('ha ha')
@@ -229,11 +231,11 @@ class GridAddressing:
             
         return
 
-
     def stop(self):
         self.stopped = True
         GPIO.cleanup()
-
+        
+###############################
 
 class HeadTrackingThreaded:
 
@@ -322,8 +324,6 @@ class HeadTrackingThreaded:
                 eye_y = (eyedist[0][1] + eyedist[1][1])/2
                 #print('eye_x: ' + str(eye_x))
                 #print('eye_y: ' + str(eye_y))
-                #LOL I changed this
-
                 
             if self.stopped:
                 self.cap.release()
@@ -534,34 +534,6 @@ def correctedAngleCalc(pixelX,pixelY):
     #print(str(phi))
     return(alpha,phi)
 
-##def naiveAngleCalc(pixelX,pixelY):
-##    pixelXMax = x_res  
-##    pixelYMax = y_res
-##    cal_depth = 0.5  #whatever depth it takes in calibration to fill the frame
-##    cal_width = 1 #ex. width of windshield
-##    cal_height = 0.5 #ex. height of windshield
-##    alpha = math.degrees(math.atan((cal_width/2)*((pixelX-pixelXMax/2)/(pixelXMax/2))/cal_depth))
-##    phi = math.degrees(math.atan((cal_height/2)*((pixelY-pixelYMax/2)/(pixelYMax/2))/cal_depth))
-##    print(str(alpha))
-##    print(str(phi))
-##    return(alpha,phi)
-
-
-    
-#define location of camera with respect to the windshield in full frame
-#width = 139 #total width of windshield
-#height = 71.5 #total height of windshield
-#cam_perp_dist = 60 #perpendicular distance from camera to windshield in cm
-#cam_spacing = 10
-#cam_x_dist = width/2 - cam_spacing/2 #horizontal distance from left edge of windshield to camera
-#^ 65 cm corresponds to the left camera with a spacing of 10 cm between the two. 
-#cam_y_dist = height/2 #camera should be centred vertically accoutning for the incline of the windshield
-#x_centreline = width/2 #half of windshield width
-#FOV = 2*math.atan((width-cam_x_dist)/cam_perp_dist)*180/math.pi #total field of view of the camera in radians
-#driver_dist = 20 #horizontal distance from driver to left camera
-#driver_h = 40 #vertical distance (height) of driver above cameras
-
-
 #Set up GLOBAL alpha, phis for use in grid addressing!
 alpha = [None,None]
 phi = [None,None]
@@ -602,32 +574,9 @@ for i in range(x_res):
     col_blk_bot.append(len(np.where(map2[:,i] < 0)[0]))
     col_blk_top.append(len(np.where(map2[:,i] > y_res)[0]))
 
-#####DEBUGGING############
-#correctedAngleCalc(0,479)
-#naiveAngleCalc(160,240)
-##########################
-
-
 
 #loop until user quits
-flag = 3 #default serial flag before loop starts
 while True:
-    #update sunlight sensor inputs (vis, IR, UV)
-    #(vis,IR,UV) = readSunLight()
-    #test prints
-    #print('Vis:' + str(vis))
-    #print('IR:' + str(IR))
-    #print('UV:' + str(UV))
-
-          
-    #set camera exposure according to sensor inputs
-    #increment = increment + 1
-    #if IR < 300:
-    #    vs.changeShutter(int(500*math.pow(1.1,increment)))
-    #elif IR < 600:
-        #vs.changeShutter(500)
-    #don't need an else case because exposure should be set to 500 as IR
-    #climbs past 600
         
     #grab frame
     frame = vs.read()#already an array
@@ -695,16 +644,7 @@ while True:
 
         (FinalX, FinalY) = undistort(cX,cY)               
 
-        #(x_windshield,y_windshield) = shift(FinalX,FinalY)
-         
-        #Convert adjusted windshield locations to angle with respect to the camera's normal
-        #position on windshield
-        #Also convert angles to degrees
-        #alpha.append(math.atan((x_windshield-cam_x_dist)/cam_perp_dist)*180/math.pi)
-        #phi.append(-math.atan((y_windshield-cam_y_dist)/cam_perp_dist)*180/math.pi) #has a negative sign to account for inverted FinalY
-
-        #using new angle calc method from calibration!
-
+        #using new angle calc method from calibration
         (temp_alpha,temp_phi) = correctedAngleCalc(FinalX,FinalY)
         alpha[i] = temp_alpha
         phi[i] = temp_phi
@@ -714,10 +654,7 @@ while True:
         print('alpha ' + str(i+1) + ': ' + str(alpha[i]))
         print('phi ' + str(i+1) + ': ' + str(phi[i]))
        
-        #for getting the angle with respect to the driver using data from both cameras,
-        #need to make sure that the two pairs of angles being worked are the right match.
-        #Can do this through either master-slave config or simple comparisons. 
-        
+
     #show the frame for debugging
     #for true calibration this would need to be 'unfisheyed'
     cv2.imshow("Frame", frame)
@@ -730,193 +667,39 @@ while True:
        
 
     #perform serial read operations if something is in the buffer
-    if ser.inWaiting():
-  
-        flag_array = []
+    if ser.inWaiting():        
+        phi1_array = []
+             
         for i in range(2):
-            flag_array.append(ser.read())
-        
-        flag_packed = flag_array[0] + flag_array[1]
-        if len(flag_packed) == 2:
-            flag_unpacked = struct.unpack('h', flag_packed)
-            flag = int(flag_unpacked[0])
+            phi1_array.append(ser.read())
+                      
+        phi1_packed = phi1_array[0] + phi1_array[1]
+        if len(phi1_packed) == 2:
+            phi1_unpacked = struct.unpack('h', phi1_packed)
+            phi1_read = phi1_unpacked[0]
+            phi_other[0] = phi1_read/100
 
-        else:
-            flag = 0 #if there is an error in serial comms, reset the loop
-                     #without doing anything
-
-
-        #print('read something: ' + str(flag))
-        #print('translated: ' +str(ord(flag)))
-        if  flag == 1:
-            #print('read 1')
-            #save current read value so it's not lost somehow after checking it
-            #if ord(current_read) != 1: #good chance that don't actually need to wait for this to happen
-            x1_array = []
-            y1_array = []
-
-            for i in range(2):
-                x1_array.append(ser.read())
-                #print(x1_array[i])
-            
-            for i in range(2):
-                y1_array.append(ser.read())
-                #print(y1_array[i])
-
-            x1_packed = x1_array[0] + x1_array[1]
-            x1_unpacked = struct.unpack('h', x1_packed)
-            x1_read = x1_unpacked[0]
-          
-            y1_packed = y1_array[0] + y1_array[1]
-            y1_unpacked = struct.unpack('h', y1_packed)
-            y1_read = y1_unpacked[0]
-
-            (FinalX_other1,FinalY_other1) = undistort(x1_read,y1_read)
-
-            #(x_windshield_other1, y_windshield_other1) = shift(FinalX_other1,FinalY_other1)
-
-            #now convert read x,y to angles
-            #note addition of cam_spacing to get correct alpha of right camera
-            #alpha_other[0] = math.atan((x_windshield_other1-cam_x_dist)/cam_perp_dist)*180/math.pi
-            #phi_other[0] = -math.atan((y_windshield_other1-cam_y_dist)/cam_perp_dist)*180/math.pi
-
-            (temp_alpha,temp_phi) = correctedAngleCalc(FinalX_other1,FinalY_other1)
-            alpha_other[0] = temp_alpha
-            phi_other[0] = temp_phi
-
-            #print('other x 1: ' + str(x1_read))
-            #print('other y 1: ' + str(y1_read))
-            print('other alpha 1: ' + str(alpha_other[0]))
-            print('other phi 1: ' + str(phi_other[0]))
-            
-
-            #change to next flag value after taking in first light
-            flag_array = []
-            for i in range(2):
-                flag_array.append(ser.read())
-            
-            flag_packed = flag_array[0] + flag_array[1]
-            flag_unpacked = struct.unpack('h', flag_packed)
-            flag = int(flag_unpacked[0])
-        
+        print('other phi 1: ' + str(phi_other[0]))           
        
-        if flag == 2:
-            print('read 2')
+        phi2_array = []
             
-            x2_array = []
-            y2_array = []
-            
-            for i in range(2):
-                x2_array.append(ser.read())
+        for i in range(2):
+            phi2_array.append(ser.read())
+
+        phi2_packed = phi2_array[0] + phi2_array[1]
+        if len(phi2_packed) == 2:
+            phi2_unpacked = struct.unpack('h', phi2_packed)
+            phi2_read = phi2_unpacked[0]           
+
+            if abs(phi2_read-phi1_read) > 100: #if difference > 1 deg
+                phi_other[1] = phi2_read/100 #separate phi for separate light        
+                print('other phi 2: ' + str(phi_other[1]))
+            else:
+                phi_other[0] = phi2_read/100 #if too close, update same light
                 
-            for i in range(2):
-                y2_array.append(ser.read())
-
-            x2_packed = x2_array[0] + x2_array[1]
-            x2_unpacked = struct.unpack('h', x2_packed)
-            x2_read = x2_unpacked[0]
-
-            y2_packed = y2_array[0] + y2_array[1]
-            y2_unpacked = struct.unpack('h', y2_packed)
-            y2_read = y2_unpacked[0]
-
-            (FinalX_other2,FinalY_other2) = undistort(x2_read,y2_read)
-
-            #(x_windshield_other2, y_windshield_other2) = shift(FinalX_other2,FinalY_other2)
-            
-            #now convert read x,y to angles
-            #note addition of cam_spacing to get correct alpha of right camera
-            #alpha_other[1] = math.atan((x_windshield_other2-(cam_x_dist + cam_spacing))/cam_perp_dist)*180/math.pi
-            #phi_other[1] = -math.atan((y_windshield_other2-cam_y_dist)/cam_perp_dist)*180/math.pi
-            #print('other alpha 2: ' + str(alpha_other[1]))
-            #print('other phi 2: ' + str(phi_other[1]))
-
-            (temp_alpha,temp_phi) = correctedAngleCalc(FinalX_other2,FinalY_other2)
-            alpha_other[1] = temp_alpha
-            phi_other[1] = temp_phi
-
-            print('other x 2: ' + str(x1_read))
-            print('other y 2: ' + str(y1_read))
-            print('other alpha 2: ' + str(alpha_other[1]))
-            print('other phi 2: ' + str(phi_other[1]))
-            
-            #change to next flag value after taking in second light
-            flag_array = []
-            for i in range(2):
-                flag_array.append(ser.read())
-            
-            flag_packed = flag_array[0] + flag_array[1]
-            flag_unpacked = struct.unpack('h', flag_packed)
-            flag = int(flag_unpacked[0])
-            
-                
-        if flag == 3:
-            print('read 3')
-
-            dx_array = []
-            dy_array = []
-            dz_array = []
-            
-            for i in range(2):
-                dx_array.append(ser.read())
-            for i in range(2):
-                dy_array.append(ser.read())
-            for i in range(2):
-                dz_array.append(ser.read())
-    
-            dx_packed = dx_array[0] + dx_array[1]
-            dx_unpacked = struct.unpack('h', dx_packed)
-            dx_read = dx_unpacked[0]
-            print('driver x:' + str(dx_read))
-
-            dy_packed = dy_array[0] + dy_array[1]
-            dy_unpacked = struct.unpack('h', dy_packed)
-            dy_read = dy_unpacked[0]
-            print('driver y:' + str(dy_read))
-
-            dz_packed = dz_array[0] + dz_array[1]
-            dz_unpacked = struct.unpack('h', dz_packed)
-            dz_read = dz_unpacked[0]/10.0 #divide by 10 since it was multiplied by 10 before being sent. 
-            print('driver z:' + str(dz_read))
-                 
-    ########HUGE OVERHAUL FOR PARALLAX IS NEEDED#################### (Useless as it is now)
-    #########SAVE FOR AFTER INTEGRATION???#########################
-
-    #parallax correction math
-    #ONLY WORKS FOR A SINGLE LIGHT FOR NOW
-    #naming conventions from module report
-    #actual trig is slightly modified
-    
-##    if len(alpha) == 1 and len(phi) == 1 and alpha_other[0] and phi_other[0]:
-##        #horizontal part:
-##        theta_c1 = (90-alpha[0])*math.pi/180 #convert each to radians
-##        theta_c2 = (90+alpha_other[0])*math.pi/180
-##        theta_SC = math.pi-theta_c1-theta_c2
-##        d_c1 = cam_spacing/math.sin(theta_SC)*math.sin(theta_c2)
-##        d = d_c1*math.sin(theta_c1) #the key perp distance to light source
-##        
-##        #now get the horizontal driver angle
-##        d_D = math.sqrt(math.pow(d_c1,2) + math.pow(driver_dist,2)-2*d_c1*driver_dist*math.cos(math.pi-theta_c1))  
-##        theta_Dx = math.asin(d_c1/d_D*math.sin(math.pi-theta_c1))
-##    
-##        #vertical part:
-##        phi_c = (phi[0] + phi_other[0])/2.0*math.pi/180 #take the average and convert to radians
-##        a = d*math.tan(phi_c)
-##        c = math.sqrt(math.pow(a,2) + math.pow(d,2))
-##        
-##        #now get the vertical driver angle
-##        s = math.sqrt(math.pow(c,2) + math.pow(driver_h,2) - 2*c*driver_h*math.cos(math.pi/2-phi_c))
-##        theta_Dy = math.asin(c/s*math.sin(math.pi/2-phi_c))
-##
-##        #convert to alpha,phi convention and to degrees
-##        alpha_D = (math.pi/2-theta_Dx)*180/math.pi
-##        phi_D = (theta_Dy - math.pi/2)*180/math.pi
-##
-##        #print('Driver alpha: ' + str(alpha_D))
-##        #print('Driver phi: ' + str(phi_D))
-
     #flush the serial buffer to avoid falling really behind 
     ser.flush()
+    ser.reset_input_buffer()
         
     #update FPS count
     fps.update()
